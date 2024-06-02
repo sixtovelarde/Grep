31c35f86ad7cd9543c1f4dd7a738bd739a87e3e0
sixtovelarde <163205281+sixtovelarde@users.noreply.github.com>
2024-06-02 11:13:45 -0400

    v.1.0.1

--- grep.cpp
+++ grep.cpp
@@ -1,3 +1,13 @@
 #include<iostream>
 #include<fstream>
 using namespace std;
+
+int main (){
+    string file = "mi_archivo.txt";
+
+    ifstream fileopen (namefile);
+
+    if (!namefile) {
+
+    }
+}
