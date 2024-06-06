242202d26d518e4a08852dcc865b23aec47bc48b
sixtovelarde <163205281+sixtovelarde@users.noreply.github.com>
2024-06-05 21:41:21 -0400

    v.1.0.1

--- grep.cpp
+++ grep.cpp
@@ -1,13 +1,44 @@
 #include<iostream>
 #include<fstream>
+#include<string>
+#include<cstring>
+#include<cstdio>
+
 using namespace std;
 
-int main (){
-    string file = "mi_archivo.txt";
+void grep(const char* pal, const char* name_pal){
 
-    ifstream fileopen (namefile);
+    ifstream carpeta(name_pal);
+    char cadena[1000];
+    int linea=0;
 
-    if (!namefile) {
+    if (carpeta){
 
+        carpeta.getline(cadena, 1000);
+        linea++;
+
+        if (carpeta && strstr(cadena, pal)){
+
+            cout<<name_pal<<"("<<linea<<")"<<cadena<<endl;
+        }
     }
+
+}
+
+int main (int argc, char* argv[]) {
+
+    if (argc < 3) {
+        cout << "Uso: grep <palabra a buscar> <carpeta1> [carpetas...]" << endl;
+        return -1;
+    } 
+    
+    else {
+        for (int i = 2; i < argc; i++) {
+            grep(argv[1], argv[i]);
+        }
+    }
+
+
+    return 0;
+
 }
