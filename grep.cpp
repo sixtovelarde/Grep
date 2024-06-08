50f6d93f5723e91da5e07fdb7ebbb18880a99869
sixtovelarde <163205281+sixtovelarde@users.noreply.github.com>
2024-06-08 08:21:14 -0400

    v.1.0.1 (listas las funciones de colorear las palabras)

--- grep.cpp
+++ grep.cpp
@@ -1,10 +1,53 @@
 #include<iostream>
 #include<fstream>
 #include<string>
+#include<sstream>
 #include<cstring>
 #include<cstdio>
+#include<windows.h>
 
 using namespace std;
+
+const int maxpalabus = 10;
+
+//Habilitar colores en la consola
+void colorear(int color){
+    HANDLE hConsola = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsola, color);
+}
+//colorear
+void ColorPalabra(string& texto, const string palcoloreadas[maxpalabus], int numpalabras){
+
+    size_t pos = 0;
+    size_t encontradas;
+
+    while ((encontradas = texto.find_first_of(" \n\t", pos)) != string::npos){
+
+        string palabra = texto.substr(pos, encontradas - pos);
+        bool resaltar = false;
+
+        for (int i=0; i < numpalabras; i++){
+
+            if (palabra == palcoloreadas[i]){
+
+                resaltar = true;
+                break;
+            }
+        }
+
+        if (resaltar){
+            colorear(FOREGROUND_BLUE| FOREGROUND_INTENSITY);
+            cout << palabra;
+            colorear(FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+        }
+        else {
+            cout << palabra;
+        }
+        cout << texto[encontradas];
+        pos = encontradas + 1;
+    }
+    cout << texto.substr(pos);
+}
 
 void grep(const char* pal, const char* name_pal){
 
