54f6fdc0cfca8dbcf7864e0dd069bae689defade
sixtovelarde <163205281+sixtovelarde@users.noreply.github.com>
2024-06-19 22:00:23 -0400

    v3.0.0 ultimate version

--- grep.cpp
+++ grep.cpp
@@ -1,56 +1,97 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <windows.h>
+#include <regex>
+#include <unordered_map>
 
 using namespace std;
 
-// Función para resaltar coincidencias en color
-void colorear(const string& palabra) {
-    HANDLE hConsola = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleTextAttribute(hConsola, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
-    cout << palabra;
-    SetConsoleTextAttribute(hConsola, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+//Imprmir instrucciones de uso
+void print_usage(const char *grep){
+
+    cout << "\n\033[1;31mUso: \033[1;33m" << grep << "\n\033[1;31m   INTRODUCIR LO SIGUIENTE EN EL ORDEN ESPECIFICO\n\n\033[1;36m  1ro- g++ -o main (nombrearchivo).cpp \033[1;35mSOLO SI ES LA 1RA VEZ QUE EJECUTAS EL PROGRAMA\033[1;36m"<<endl;
+    cout << "\n  2do- ./grep.exe (ruta COMPLETA del archivo) (palabra a buscar) (sensibilidad a mayusculas) (color resaltado)" << endl;
+    cout << "       \033[1;31m\nSensibilidad a mayusculas:\033[1;36m 1 para distinguir entre mayusculas y minusculas, 0 para ignorar" << endl;
+    cout << "       \033[1;31mColor resaltado:\033[1;36m (opciones: rojo, verde, azul, amarillo, magenta, cian)\n\033[1;35m     Recuerda cambiar los slags a '\033[1;33m/\033[1;35m'\033[0m\n" << endl;
 }
 
-// Función para buscar coincidencias en un archivo
-void buscarCoincidencias(const string& archivo, const string& palabra) {
-    ifstream archivoEntrada(archivo, fstream::in); // Cambio para usar el parámetro 'archivo'
-    if (!archivoEntrada.is_open()) {
-        cout << "Error al abrir el archivo." << endl;
-        return;
+//declarar colores
+string declararColores(const string &color){
+
+    unordered_map<string, string> tonos = {
+
+        {"rojo", "\033[1;31m"},
+        {"verde", "\033[1;32m"},
+        {"azul", "\033[1;34m"},
+        {"amarillo", "\033[1;33m"},
+        {"magenta", "\033[1;35m"},
+        {"cian", "\033[1;36m"}
+        };
+
+    if (tonos.find(color) != tonos.end()){
+        return tonos[color];
     }
 
-    string linea;
-    int numLineas = 0;
-    int numCoincidencias = 0;
+    else{
 
-    while (getline(archivoEntrada, linea)) {
-        size_t pos = linea.find(palabra);
-        while (pos != string::npos) {
-            numCoincidencias++;
-            cout << linea.substr(0, pos); // Imprime el texto antes de la coincidencia
-            colorear(linea.substr(pos, palabra.length())); // Resalta la coincidencia
-            linea = linea.substr(pos + palabra.length()); // Actualiza la línea para continuar la búsqueda
-            pos = linea.find(palabra); // Busca la siguiente coincidencia
-        }
-        cout << linea << endl; // Imprime el resto de la línea
-        numLineas++;
+        cout << "Color no valido. Usando color rojo por defecto." << endl;
+        return "\033[1;31m";
     }
-    cout << "Total de lineas con coincidencias: " << numLineas << endl;
-    cout << "Total de coincidencias encontradas: " << numCoincidencias << endl;
 }
 
-int main(int argc, char* argv[]) {
-    if (argc != 3) {
-        cout << "Su uso: " << argv[0] << " 'archivo' 'palabra'" << endl;
+
+//colorear palabras
+void PonerlecolorPals(const string &line, const string &word, bool case_sensitive, const string &color_code){
+    regex_constants::syntax_option_type flag = case_sensitive ? regex_constants::ECMAScript : regex_constants::icase;
+    regex word_regex("(" + word + ")", flag);
+    string result = regex_replace(line, word_regex, color_code + "$&\033[0m");
+    cout << result << endl;
+}
+
+int main(int argc, char *argv[]){
+
+    if (argc != 5){
+        print_usage(argv[0]);
         return 1;
     }
 
-    string archivo = argv[1];
-    string palabra = argv[2];
+    //opciones para los argumentos
+    string file_path = argv[1];
+    string search_word = argv[2];
+    bool case_sensitive = stoi(argv[3]);
+    string color = argv[4];
+    string color_code = declararColores(color);
 
-    buscarCoincidencias(archivo, palabra);
+    ifstream file(file_path);
+
+    if (!file.is_open()){
+
+        cout << "Error: No se pudo abrir el archivo " << file_path << endl;
+        return 1;
+    }
+
+    string line;
+    int nrodelineas = 0;
+    int total_coincidencias = 0;
+    int lineasCon_coincidencias = 0;
+
+    while (getline(file, line)){
+
+        nrodelineas++;
+        regex_constants::syntax_option_type flag = case_sensitive ? regex_constants::ECMAScript : regex_constants::icase;
+        regex word_regex(search_word, flag);
+
+        if (regex_search(line, word_regex)){
+
+            PonerlecolorPals(line, search_word, case_sensitive, color_code);
+            total_coincidencias++;
+            lineasCon_coincidencias++;
+        }
+    }
+
+    cout << "Total de coincidencias: " << total_coincidencias << endl;
+    cout << "Total de lineas con coincidencias: " << lineasCon_coincidencias << endl;
+    file.close();
 
     return 0;
 }
